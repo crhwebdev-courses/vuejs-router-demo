d036438355f71736b6abaea04874b58b7eafd5e7
Carl Hain <crhain@gmail.com>
2020-03-25 12:05:02 -0500

    Add exact routing to home route in Header component to ensure that router sets active class correctly

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="nav nav-pills">
-    <router-link class="nav-item" tag="li" active-class="active" to="/"
+    <router-link class="nav-item" to="/" tag="li" active-class="active" exact
       ><a class="nav-link">Home</a></router-link
     >
-    <router-link class="nav-item" tag="li" active-class="active" to="/user"
+    <router-link to="/user" class="nav-item" tag="li" active-class="active"
       ><a class="nav-link">User</a></router-link
     >
   </ul>

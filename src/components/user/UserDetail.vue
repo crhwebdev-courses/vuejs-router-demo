e19716aebca0a974480c7bbcce83b02c782becea
Carl Hain <crhain@gmail.com>
2020-03-25 15:06:27 -0500

    Add edit button to UserDetail and make it link to the UserEdit component

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h3>Some User Details</h3>
-    <p>User ID: {{ id }}</p>
+    <p>User loaded has ID: {{ id }}</p>
+    <router-link
+      class="btn btn-primary"
+      tag="button"
+      :to="`/user/${$route.params.id}/edit`"
+      >Edit User</router-link
+    >
   </div>
 </template>
 <script>

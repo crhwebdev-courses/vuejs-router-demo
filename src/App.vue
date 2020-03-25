3d0af66f871fdf7bc559ef05bee910bf6c35bcc0
Carl Hain <crhain@gmail.com>
2020-03-25 11:56:21 -0500

    Add Header component to app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Routing</h1>
+        <app-header></app-header>
         <hr />
         <router-view></router-view>
       </div>
@@ -11,7 +11,12 @@
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
 <style></style>

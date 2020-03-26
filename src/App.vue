1258fe0a5f03f2f7f3c858eb16e0aecc521c4bed
Carl Hain <crhain@gmail.com>
2020-03-26 16:31:07 -0500

    Add named router views so we can include a Header at top of Home page and at bottom of User page

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <app-header></app-header>
+        <h1>Routing</h1>
         <hr />
+        <router-view name="header-top"></router-view>
         <router-view></router-view>
+        <router-view name="header-bottom"></router-view>
       </div>
     </div>
   </div>
